476c2eaf575bd3c75333b775de9566842972515b
Srayoshi Ghosh <srayoshighosh@Srayoshis-MacBook-Pro.local>
2022-02-17 01:56:15 +0600

    bug fixed

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ app.post("/failure", (req, res) => {
   res.redirect("/");
 });
 
-app.listen(process.env.port || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
